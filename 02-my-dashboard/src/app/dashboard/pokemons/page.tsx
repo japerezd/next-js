4a7a99b6e5dd21257434bf5f19e6519a30253e26
Jorge Perez <jorge_alberto1997@hotmail.com>
2024-09-04 22:25:11 -0600

    Add pokemons images

--- 02-my-dashboard/src/app/dashboard/pokemons/page.tsx
+++ 02-my-dashboard/src/app/dashboard/pokemons/page.tsx
@@ -1,8 +1,37 @@
+import Image from 'next/image';
+import { PokemonsResponse, SimplePokemon } from '../pokemons/index';
 
-export default function PokemonsPage() {
+const getPokemons = async (
+  limit = 20,
+  offset = 0
+): Promise<SimplePokemon[]> => {
+  const data = await fetch(
+    `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
+  );
+  const response: PokemonsResponse = await data.json();
+
+  const pokemons = response.results.map((pokemon) => ({
+    id: pokemon.url.split('/').at(-2)!,
+    name: pokemon.name,
+  }));
+  return pokemons;
+};
+
+export default async function PokemonsPage() {
+  const pokemons = await getPokemons(150);
   return (
-    <div>
-      <h1>Hello Page Pokemon</h1>
+    <div className="flex flex-col ">
+      <div className="flex flex-wrap gap-10 items-center justify-center">
+        {pokemons.map((pokemon) => (
+          <Image
+            key={pokemon.id}
+            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`}
+            width={100}
+            height={100}
+            alt={pokemon.name}
+          />
+        ))}
+      </div>
     </div>
   );
 }
